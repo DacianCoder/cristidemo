c4e549b4354a062528a5869013b987d2b1a73971
Cristian Boza <boza.cristian27@gmail.com>
2021-03-01 23:38:35 +0200

    wip

--- src/components/InstallPWA.tsx
+++ src/components/InstallPWA.tsx
@@ -1,27 +1,34 @@
 import React, { useEffect, useState } from 'react'
 import DynamicFormattedMessage from './common/ui/DynamicFormattedMessage'
 import Button from './common/ui/button/Button'
-
+import { promptInstall } from '../../public/worker'
 
 const InstallPWA = () => {
   const [supportsPWA, setSupportsPWA] = useState(false)
-  const [promptInstall, setPromptInstall] = useState<any>(null)
+  const [prompt, setPromptInstall] = useState<any>(null)
+  const [timer, setTimer] = useState<NodeJS.Timeout | null>(null)
 
   useEffect(() => {
-    const handler = (e: any) => {
-      e.preventDefault()
-      console.log('PWA install allowed')
-      setSupportsPWA(true)
-      setPromptInstall(e)
-    }
-    window.addEventListener('beforeinstallprompt', handler)
+    setTimer(setInterval(() => {
+      if (promptInstall) {
+        console.log(promptInstall)
+        setPromptInstall(promptInstall)
+        setSupportsPWA(true)
+        setTimer(t => {
+          t && clearInterval(t)
+          return null
+        })
+      }
+    }, 1000))
 
-    return () => window.removeEventListener('transitionend', handler)
+    return () => {
+      timer && clearInterval(timer)
+    }
   }, [])
 
   const onClick = (evt: any) => {
     evt.preventDefault()
-    promptInstall?.prompt()
+    prompt?.prompt()
   }
 
   if (!supportsPWA) {
